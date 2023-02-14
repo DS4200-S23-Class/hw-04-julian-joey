f619d38857823b9e36a0fa0b54e68348d5d074f7
Julian Savini <juliansavini0@gmail.com>
2023-02-14 14:15:15 -0500

    progress

--- hw4main.js
+++ hw4main.js
@@ -1,13 +1,13 @@
 console.log("linked");
 
-var el_up = document.getElementsByClassName("point");
-
-
-
-
-
-function gfg_Run() {
-    document.body.style.stroke = "green";
-
+function addBorder(id, x1, x2) {
+    if (document.getElementById(id).style.stroke == "green"){
+        document.getElementById(id).style.stroke = "none";
+    }
+    else {
+        document.getElementById(id).style.stroke = "green";
+    }
+    var text = "(" + String((parseInt(document.getElementById(id).getAttribute('cx')) - 20) / 20) + "," + String((parseInt(document.getElementById(id).getAttribute('cy')) + 300) / 20) + ")";
+    document.getElementById("lastPoint").textContent = text;
 }
 
