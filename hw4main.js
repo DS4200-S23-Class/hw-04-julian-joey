5ef61e164a8bfe967a5c6b56fd5a30f402789542
Julian Savini <juliansavini0@gmail.com>
2023-02-14 17:21:32 -0500

    nearing the end

--- hw4main.js
+++ hw4main.js
@@ -7,7 +7,14 @@ function addBorder(id, x1, x2) {
     else {
         document.getElementById(id).style.stroke = "green";
     }
-    var text = "(" + String((parseInt(document.getElementById(id).getAttribute('cx')) - 20) / 20) + "," + String((parseInt(document.getElementById(id).getAttribute('cy')) + 300) / 20) + ")";
+    var text = "(" + String((parseInt(document.getElementById(id).getAttribute('cx')) - 20) / 20) + "," + String((parseInt(document.getElementById(id).getAttribute('cy')) - 300) / -20) + ")";
     document.getElementById("lastPoint").textContent = text;
-}
+};
 
+function plotPoints() {
+    var x = document.getElementById("xDropdown").value;
+    var y = document.getElementById("yDropdown").value;
+    if (x != "none" || y != "none") {
+        //Insert circle with coordinates
+    }
+};
