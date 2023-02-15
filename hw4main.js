b89621271072280995961477be47e76ecf8e8f71
Julian Savini <juliansavini0@gmail.com>
2023-02-14 22:50:59 -0500

    final

--- hw4main.js
+++ hw4main.js
@@ -1,13 +1,14 @@
 console.log("linked");
 
-function addBorder(id, x1, x2) {
+function addBorder(id) {
     if (document.getElementById(id).style.stroke == "green"){
         document.getElementById(id).style.stroke = "none";
     }
     else {
         document.getElementById(id).style.stroke = "green";
+        document.getElementById(id).style.strokeWidth = "3px"
     }
-    var text = "(" + String((parseInt(document.getElementById(id).getAttribute('cx')) - 20) / 20) + "," + String((parseInt(document.getElementById(id).getAttribute('cy')) - 300) / -20) + ")";
+    var text = "(" + String((parseInt(document.getElementById(id).getAttribute('cx')) - 150) / 40) + "," + String((parseInt(document.getElementById(id).getAttribute('cy')) - 500) / -40) + ")";
     document.getElementById("lastPoint").textContent = text;
 };
 
@@ -16,5 +17,9 @@ function plotPoints() {
     var y = document.getElementById("yDropdown").value;
     if (x != "none" || y != "none") {
         //Insert circle with coordinates
+        document.getElementById("p5").setAttribute("r", 5);
+        document.getElementById("p5").setAttribute("cx", (x * 40) + 150);
+        document.getElementById("p5").setAttribute("cy", 500 - (y * 40));
+        document.getElementById("p5").style.visibility = "visible";
     }
 };
